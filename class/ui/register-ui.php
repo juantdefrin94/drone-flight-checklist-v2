58f8996e61e6be614f6c656bc4a9a960b4189a15
irennaviolla <irennaviollan@gmail.com>
2024-04-16 11:23:01 +0700

    Adding Register UI

--- class/ui/register-ui.php
+++ class/ui/register-ui.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'class/ui/master-ui.php';
-class LoginUI extends MasterUI{
+class RegisterUI extends MasterUI{
 
     private $db = null;
     private $view = <<<HTML
@@ -10,9 +10,9 @@ class LoginUI extends MasterUI{
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="styles/login-style.css">
+            <link rel="stylesheet" href="">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <title>Login Page</title>
+            <title>Register Page</title>
         </head>
         <body>
             <div class="container">
@@ -20,8 +20,14 @@ class LoginUI extends MasterUI{
                     <img class="background-login" src="assets/login-background.png" alt="">
                 </div>
                 <form class="login-container" method="POST" action="action/login.php">
-                    <div class="login-title">Drone</br>Checking System</div>
-                    <div class="login-title-no-bold ">Login to Your Account</div>
+                    <div class="login-title-no-bold ">Register Your Account</div>
+                    <div class="label-input-container">
+                        <label for="" class="label-input">Email</label>
+                        <div class="container-input">
+                            <i class='fa-solid fa-user fa-lg' style='color:#d69883; margin-right:10px;'></i>
+                            <input class="input-style" type="text" name="email" placeholder="example123@gmail.com">
+                        </div>
+                    </div>
                     <div class="label-input-container">
                         <label for="" class="label-input">Username</label>
                         <div class="container-input">
@@ -36,10 +42,10 @@ class LoginUI extends MasterUI{
                             <input class="input-style" type="password" name="password" placeholder="**********">
                         </div>
                     </div>
-                    <div><input class="login-button" type="submit" value="Log In"></div>
+                    <div><input class="" type="submit" value="Register"></div>
                 </form>
                 <div>
-                    Don't have account? <span><a onclick="">Register here</a></span> 
+                    Already Have An Accpunt? <span><a onclick="">Login here</a></span> 
                 </div>
             </div>
         </body>
