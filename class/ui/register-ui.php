d6ccc5ae1cf820b11d45f16d5a395fcd197c3ebd
irennaviolla <irennaviollan@gmail.com>
2024-05-16 11:31:15 +0700

    Adding CSS for login and register

--- class/ui/register-ui.php
+++ class/ui/register-ui.php
@@ -8,50 +8,55 @@ class RegisterUI{
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="">
+            <link rel="stylesheet" href="styles/register-style.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <title>Register Page</title>
         </head>
         <body>
             <div class="container">
-                <div class="image-container">
-                    <img class="background-login" src="assets/login-background.png" alt="">
+                <div class="register-box-left">
+                    <div class="register-title">Drone<br>Checking System</div>
+                        <form class="register-container" method="POST">
+                        <div class="register-statement ">Register Your Account</div>
+                        <div class="label-register-container">
+                            <label class="label-register">Email</label>
+                            <div class="container-register">
+                                <i class='fa-solid fa-user fa-lg' style='color:#5D7C7E; margin-right:15px;'></i>
+                                <input class="register-style" type="text" name="email" placeholder="example123@gmail.com">
+                            </div>
+                        </div>
+                        <div class="label-register-container">
+                            <label class="label-register">Username</label>
+                            <div class="container-register">
+                                <i class='fa-solid fa-user fa-lg' style='color:#5D7C7E; margin-right:15px;'></i>
+                                <input class="register-style" type="text" name="username" placeholder="example123">
+                            </div>
+                        </div>
+                        <div class="label-register-container">
+                            <label for="" class="label-register">Password</label>
+                            <div class="container-register">
+                                <i class='fa-solid fa-lock fa-lg' style='color:#5D7C7E; margin-right:15px;'></i>
+                                <input class="register-style" type="password" name="password" placeholder="**********">
+                            </div>
+                        </div>
+                        <div class="label-register-container">
+                            <label for="" class="label-register">Confirm Password</label>
+                            <div class="container-register">
+                                <i class='fa-solid fa-lock fa-lg' style='color:#5D7C7E; margin-right:15px;'></i>
+                                <input class="register-style" type="password" name="confirmPassword" placeholder="**********">
+                            </div>
+                        </div>
+                        <div><input class="register-button" type="submit" value="Register"></div>
+                        <div class="login-link">
+                            Already Have An Account? <span><a href="index.php?view=login">Login here</a></span> 
+                        </div>
+                    </form>
                 </div>
-                <form class="login-container" method="POST">
-                    <div class="login-title-no-bold ">Register Your Account</div>
-                    <div class="label-input-container">
-                        <label for="" class="label-input">Email</label>
-                        <div class="container-input">
-                            <i class='fa-solid fa-user fa-lg' style='color:#d69883; margin-right:10px;'></i>
-                            <input class="input-style" type="text" name="email" placeholder="example123@gmail.com">
-                        </div>
-                    </div>
-                    <div class="label-input-container">
-                        <label for="" class="label-input">Username</label>
-                        <div class="container-input">
-                            <i class='fa-solid fa-user fa-lg' style='color:#d69883; margin-right:10px;'></i>
-                            <input class="input-style" type="text" name="username" placeholder="example123">
-                        </div>
-                    </div>
-                    <div class="label-input-container">
-                        <label for="" class="label-input">Password</label>
-                        <div class="container-input">
-                            <i class='fa-solid fa-lock fa-lg' style='color:#d69883; margin-right:10px;'></i>
-                            <input class="input-style" type="password" name="password" placeholder="**********">
-                        </div>
-                    </div>
-                    <div class="label-input-container">
-                        <label for="" class="label-input">Confirm Password</label>
-                        <div class="container-input">
-                            <i class='fa-solid fa-lock fa-lg' style='color:#d69883; margin-right:10px;'></i>
-                            <input class="input-style" type="password" name="confirmPassword" placeholder="**********">
-                        </div>
-                    </div>
-                    <div><input class="" type="submit" value="Register"></div>
-                </form>
-                <div>
-                    Already Have An Accpunt? <span><a href="index.php?view=login">Login here</a></span> 
+
+                <div class="register-box-right">
+                    <img class="register-image" src="asset/main-image.jpg" alt="">
                 </div>
+                
             </div>
         </body>
         </html>
