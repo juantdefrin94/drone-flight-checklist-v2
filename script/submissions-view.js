f3133492de9f027719510a59690cffeb356b5ee7
juantdefrin94 <jtdefrin33@gmail.com>
2024-05-17 15:12:33 +0700

    add new function for expansion answer

--- script/submissions-view.js
+++ script/submissions-view.js
@@ -1,11 +1,10 @@
-var initVal = "";
-var maxPre = 0;
-var maxPost = 0;
+let groupNum = 1;
 
 $(document).ready(function () {
 
     function init(){
         loadData();
+        groupEvent();
     }
 
     function loadData(){
@@ -16,120 +15,83 @@ $(document).ready(function () {
         let pre = $json.formData.find((item) => item.type === "pre");
         let post = $json.formData.find((item) => item.type === "post");
 
-        let $type = $('#type');
-
         if(assessment){
             initVal = "assessment";
-            $type.append(new Option("Assessment", "assessment"));
             let length = assessment.answer.length;
 
-            let $assessment = $('#assessment-answer')
+            let $assessmentAnswer = $('#assessment-answer');
+            let answerString = `<div><div class='group' id='group-${groupNum}'><div class='group-title'>Assessment</div><div class='drop-symbol' id='symbol-${groupNum}'><i class='fa-solid fa-caret-up'></i></div><div id='group-${groupNum}-answer' style='display: none;'>`;
             for(let i = 0; i < length; i++){
-                $assessment.append(`<p>${assessment.answer[i].questionName} : ${assessment.answer[i].answer}</p>`)
+                answerString += `<div>${assessment.answer[i].questionName} : ${assessment.answer[i].answer}</div>`
             }
+            answerString += "</div></div>";
+            $assessmentAnswer.append(answerString);
+            groupNum++;
         }
 
         if(pre){
             if(initVal == ""){
                 initVal = "pre";
             }
-            let $pre = $('#pre-answer');
             let preLength = pre.answer.length;
             maxPre = preLength;
+            let $preAnswer = $('#pre-answer');
             for(let i = 1; i <= preLength; i++){
                 let text = "Pre-Flight #" + i;
-                let value = "pre-" + i;
-                $type.append(new Option(text, value));
-
-                let appendText = `<div id="pre-answer-${i}" style="display: none">`;
+                
+                let answerString = `<div><div class='group' id='group-${groupNum}'><div class='group-title'>${text}</div><div class='drop-symbol' id='symbol-${groupNum}'><i class='fa-solid fa-caret-up'></i></div><div id='group-${groupNum}-answer' style='display: none;'>`;
                 let length = pre.answer[i - 1].data.length;
                 for(let j = 0; j < length; j++){
-                    appendText += `<p>${pre.answer[i - 1].data[j].questionName} : ${pre.answer[i - 1].data[j].answer}</p>`;
+                    answerString += `<div>${pre.answer[i - 1].data[j].questionName} : ${pre.answer[i - 1].data[j].answer}</div>`
                 }
-                appendText += `</div>`;
-                $pre.append(appendText);
+                answerString += "</div></div>";
+                $preAnswer.append(answerString);
+                groupNum++;
             }
         }
         
-        let $post = $('#post-answer');
         if(post){
             if(initVal == ""){
                 initVal = "post";
             }
             let postLength = post.answer.length;
             maxPost = postLength;
+            let $postAnswer = $('#post-answer');
             for(let i = 1; i <= postLength; i++){
                 let text = "Post-Flight #" + i;
-                let value = "post-" + i;
-                $type.append(new Option(text, value));
 
-                let appendText = `<div id="post-answer-${i}" style="display: none">`;
+                let answerString = `<div><div class='group' id='group-${groupNum}'><div class='group-title'>${text}</div><div class='drop-symbol' id='symbol-${groupNum}'><i class='fa-solid fa-caret-up'></i></div><div id='group-${groupNum}-answer' style='display: none;'>`;
                 let length = post.answer[i - 1].data.length;
                 for(let j = 0; j < length; j++){
-                    appendText += `<p>${post.answer[i - 1].data[j].questionName} : ${post.answer[i - 1].data[j].answer}</p>`;
+                    answerString += `<div>${post.answer[i - 1].data[j].questionName} : ${post.answer[i - 1].data[j].answer}</div>`
                 }
-                appendText += `</div>`;
-                $post.append(appendText);
+                answerString += "</div></div>";
+                $postAnswer.append(answerString);
+                groupNum++;
             }
         }
-
-        setOnChangeSelect($type);
+        
     }
 
-    function setOnChangeSelect($selection){
-        showAnswer(initVal);
-        $selection.on('change', function (){
-            let value = this.value;
-            showAnswer(value);
-        })
-    }
+    function groupEvent(){
+        let $group = $('.group');
+        let length = $group.length;
+        for(let i = 0; i < length; i++){
+            let groupId = $group[i].id;
+            let symbolId = "symbol-" + groupId.split("-")[1];
 
-    function showAnswer(value){
-        let ansId = "";
-        if(value === "assessment"){
-            ansId = "#assessment-answer";
-            hideOther("assessment", 0);
-        }else if(value.includes("pre")){
-            let id = value.split("-")[1];
-            ansId = "#pre-answer-" + id;
-            hideOther("pre", id);
-        }else{
-            let id = value.split("-")[1];
-            ansId = "#post-answer-" + id;
-            hideOther("post", id);
-        }
-
-        $(ansId).css("display", "block");
-    }
-
-    function hideOther(value, id){
-        if(value == "assessment"){
-            for(let i = 1; i <= maxPre; i++){
-                $("#pre-answer-" + i).css("display", "none");
-            }
-            for(let i = 1; i <= maxPost; i++){
-                $("#post-answer-" + i).css("display", "none");
-            }
-        }else if(value == "pre"){
-            $("#assessment-answer").css("display", "none");
-            for(let i = 1; i <= maxPre; i++){
-                if(i !== id){
-                    $("#pre-answer-" + i).css("display", "none");
+            $('#' + groupId).on('click', function (){
+                let $symbolEl = $('#' + symbolId);
+                let groupAnswerString = groupId + "-answer";
+                let $groupAnswer = $('#' + groupAnswerString);
+                if($symbolEl.hasClass('group-open')){
+                    $symbolEl.removeClass('group-open');
+                    $groupAnswer.css('display', 'none');
+                }else{
+                    $symbolEl.addClass('group-open');
+                    $groupAnswer.css('display', 'block');
                 }
-            }
-            for(let i = 1; i <= maxPost; i++){
-                $("#post-answer-" + i).css("display", "none");
-            }
-        }else{
-            $("#assessment-answer").css("display", "none");
-            for(let i = 1; i <= maxPre; i++){
-                $("#pre-answer-" + i).css("display", "none");
-            }
-            for(let i = 1; i <= maxPost; i++){
-                if(i !== id){
-                    $("#post-answer-" + i).css("display", "none");
-                }
-            }
+            })
         }
     }
 
